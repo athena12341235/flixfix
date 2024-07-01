0ac06cf927df13ac862330ff94dc12a27fe964d6
athena12341235 <146287797+athena12341235@users.noreply.github.com>
2024-07-01 17:00:44 -0400

    Update openai.py

--- openai.py
+++ openai.py
@@ -28,36 +28,3 @@ def get_movie_recommendation(films, ratings, genre, age_rating, year_range):
               f"and movies released in the years {year_range} inclusive."
               f"Please only output a numbered list of 5 movies and a sentence-long plot summary for each.")
     return ask_ai(prompt)
-
-'''Dummy Data'''
-# films=['Toy Story',
-# 'Finding Nemo',
-# 'Monsters, Inc.',
-# 'The Incredibles',
-# 'Up']
-
-# ratings=[8,9,7,8,10]
-
-films=['Pulp Fiction',
-'The Shawshank Redemption',
-'Fight Club',
-'Forrest Gump',
-'The Matrix'
-]
-
-ratings=[8,10,7,9,6]
-
-
-# print(ask_ai(prompt))
-# #prompt= f"Using this list of films watched by a user: {films} and a list of the ratings out of 10 of they gave these films: {ratings}. Can you give a movie recommendation? Just return the name of the movie."
-
-
-
-
-
-
-
-
-
-
-
